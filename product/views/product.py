d40c182919add03bb3cbe1254457ab1a841a4310
shuvro-baset <md.abdul.baset75@gmail.com>
2022-04-06 00:56:05 +0600

    dsfa

--- product/views/product.py
+++ product/views/product.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.shortcuts import render, redirect
 from product.models import Variant, ProductVariantPrice, ProductVariant
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 class CreateProductView(generic.TemplateView):
@@ -55,3 +56,36 @@ class ProductsView(generic.TemplateView):
             "styles": styles,
         }
         return render(request, self.template_name, context=context)
+
+    # post method for filtering
+    def post(self, request):
+        if request.method == "POST":
+            # getting filter data
+            name = request.POST.get('title')
+            price_from = request.POST.get('price_from')
+            price_to = request.POST.get('price_to')
+            variant = request.POST.get('variant')
+            product_ins = ProductVariantPrice.objects.all()
+            # condition for not provided price
+            if None or '' in [price_to, price_from, variant]:
+                products = product_ins.filter(product__title__icontains=name)
+                paginator = Paginator(products, 10)
+                page_number = request.GET.get("page", 1)
+                page_obj = paginator.get_page(page_number)
+                print("filter by name: ", page_obj)
+                context = {
+                    'page_obj': page_obj
+                }
+                return render(request, 'products/filtered_products.html', context=context)
+            else:
+                products = product_ins.filter(
+                    Q(product__title__icontains=name) | Q(price__gte=price_from) | Q(price__lte=price_to))
+
+                paginator = Paginator(products, 10)
+                page_number = request.GET.get("page", 1)
+                page_obj = paginator.get_page(page_number)
+
+                context = {
+                    'page_obj': page_obj
+                }
+                return render(request, 'products/filtered_products.html', context=context)
